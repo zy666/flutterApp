4099bd5cf2f0df4961da305bbb9d840216e0850e
zhouyou <2218803995@qq.com>
2019-06-04 18:06:12 +0800

    Container widget示例2

--- lib/ContainerWidget.dart
+++ lib/ContainerWidget.dart
@@ -8,18 +8,28 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Container Widget的使用',
       home: Scaffold(
+        backgroundColor: Colors.lightGreen[100],
         body: Center(
           child: Container(
-
             child: new Text(
               'container widget',
               style: TextStyle(fontSize: 20.0),
             ),
-            alignment: Alignment.center,//居中对齐
+            alignment: Alignment.topLeft,
+            //居中对齐
+//            alignment: Alignment.center,//居中对齐
 //            alignment: Alignment.bottomCenter, //下对齐居中
-          width: 180.0,
-          height: 200.0,
-          color: Colors.lightGreen[500],
+            width: 500.0,
+            height: 400.0,
+//            color: Colors.lightGreen[500],
+            //内边距
+            padding: const EdgeInsets.fromLTRB(20.0, 10.0, 0, 100.0),
+            //外边距
+            margin: const EdgeInsets.all(10.0),
+            decoration: new BoxDecoration(
+                gradient: const LinearGradient(
+                    colors: [Colors.lightBlue, Colors.green, Colors.red]),
+                border: Border.all(width: 12.0, color: Colors.red)),
           ),
         ),
       ),
